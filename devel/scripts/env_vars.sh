bf757e55795c6f003af907af52c2ff95c94207ae
Adonis Ling <adonis0147@gmail.com>
2022-05-05 19:18:49 +0800

    Export GCONV_PATH to fix issues with gdb

--- devel/scripts/env_vars.sh
+++ devel/scripts/env_vars.sh
@@ -5,6 +5,7 @@ export DEVEL_HOME_PATH="${DEVEL_HOME_PATH:-${HOME}/devel}"
 export PKG_CONFIG_PATH="${DEVEL_HOME_PATH}/lib/pkgconfig:${PKG_CONFIG_PATH}"
 export ACLOCAL_PATH="${DEVEL_HOME_PATH}/share/aclocal:${ACLOCAL_PATH}"
 
+export GCONV_PATH="${DEVEL_HOME_PATH}/compiler/x86_64-pc-linux-gnu/sysroot/usr/lib/gconv"
 export CPATH="${DEVEL_HOME_PATH}/include"
 export LD_RUN_PATH="${DEVEL_HOME_PATH}/lib:${LD_RUN_PATH}"
 export PATH="${DEVEL_HOME_PATH}/bin:${DEVEL_HOME_PATH}/compiler/bin:${PATH}"
