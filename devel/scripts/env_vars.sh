489d2dbbf832890020d25c7e45b3d1acf23696b1
Adonis Ling <adonis0147@gmail.com>
2022-05-12 15:06:34 +0800

    Add a function: setup_locale

--- devel/scripts/env_vars.sh
+++ devel/scripts/env_vars.sh
@@ -24,3 +24,28 @@ function relocate() {
 	patchelf --set-rpath "$(dirname "${libc_so}")" "${program}"
 	patchelf --set-interpreter "${interpreter}" "${program}"
 }
+
+function setup_locale() {
+	local charmap="${1}"
+	local input="${2}"
+	local output_path="${3}"
+
+	export I18NPATH="${DEVEL_HOME_PATH}/compiler/x86_64-pc-linux-gnu/sysroot/usr/share/i18n"
+	export LOCPATH="${HOME}/.locale"
+
+	local full_ouput_path="${LOCPATH}/${output_path}"
+	if [[ ! -d "${full_ouput_path}" ]]; then
+		mkdir -p "${LOCPATH}"
+		local message="Generate the locale \033[34;1m${output_path}\033[0m to \033[34;1m${LOCPATH}\033[0m."
+		echo -e "${message}"
+
+		if ! localedef -f "${charmap}" -i "${input}" "${full_ouput_path}"; then
+			return
+		else
+			echo -e "Success!\n\033[32;1m\n\tLANG=${output_path}\n\tLANGUAGE=${output_path}\033[0m"
+		fi
+	fi
+
+	export LANG="${output_path}"
+	export LANGUAGE="${output_path}"
+}
