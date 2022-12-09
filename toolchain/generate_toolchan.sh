a6d4fe6b0938a69de168d17b4c611f890b0f433f
Adonis Ling <adonis0147@gmail.com>
2022-12-09 15:43:47 +0800

    Add a new way to generate the toolchain

--- toolchain/generate_toolchan.sh
+++ toolchain/generate_toolchan.sh
@@ -2,6 +2,24 @@
 
 set -e
 
+declare -r CMD="${BASH_SOURCE[0]}"
+WORKSPACE_PATH="$(dirname "$(readlink -f "${CMD}")")"
+declare -r WORKSPACE_PATH
+
+declare -r PACKAGES_PATH="${WORKSPACE_PATH}/packages"
+
+declare -r BINUTILS_PACKAGE_URL="https://ftpmirror.gnu.org/binutils/binutils-2.39.tar.xz"
+declare -r BINUTILS_MD5SUM='f7e986ae9ff06405cafb2e585ee36d27'
+
+declare -r LINUX_PACKAGE_URL="https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.0.12.tar.xz"
+declare -r LINUX_MD5SUM='44933812ad926f5000f01ac108d41ee8'
+
+declare -r GLIBC_PACKAGE_URL="https://ftpmirror.gnu.org/glibc/glibc-2.36.tar.xz"
+declare -r GLIBC_MD5SUM='00e9b89e043340f688bc93ec03239b57'
+
+declare -r GCC_PACKAGE_URL="https://ftpmirror.gnu.org/gcc/gcc-12.2.0/gcc-12.2.0.tar.xz"
+declare -r GCC_MD5SUM='73bafd0af874439dcdb9fc063b6fb069'
+
 function log() {
 	local level="${1}"
 	local message="${2}"
@@ -33,41 +51,304 @@ function log_error() {
 	exit 1
 }
 
-function prepare() {
-	cd /x-tools
-	mv /x-tools/x86_64-pc-linux-gnu /output
-	chmod -R u+w /output/x86_64-pc-linux-gnu
+function setup_environment() {
+	declare -gr PREFIX="${PREFIX:-/opt/toolchain}"
+	declare -gr CROSS_PREFIX="${PREFIX}/cross"
+	declare -gr TARGET="${TARGET:-x86_64-linux-gnu}"
+	declare -gr TARGET_PREFIX="${CROSS_PREFIX}/${TARGET}"
 
-	mkdir patchelf
-	tar -C patchelf -zxf patchelf.tar.gz
+	if [[ "${TARGET}" == 'x86_64-linux-gnu' ]]; then
+		declare -gr ARCH='x86'
+	elif [[ "${TARGET}" == 'aarch64-linux-gnu' ]]; then
+		declare -gr ARCH='arm64'
+	fi
+	export PATH="${PREFIX}/bin:${CROSS_PREFIX}/bin:${PATH}"
+}
 
-	mv patchelf/bin/patchelf /output/x86_64-pc-linux-gnu/bin
+function show_environment() {
+	log_info "Environment Information:"
+	log_info "  PREFIX: \033[35;1m${PREFIX}\033[0m"
+	log_info "  TARGET: \033[35;1m${TARGET}\033[0m"
+	log_info "  ARCH:   \033[35;1m${ARCH}\033[0m"
+	log_info "  PATH:   \033[35;1m${PATH}\033[0m"
+}
+
+function download() {
+	local url="${1}"
+	local md5sum="${2}"
+	local package
+	package="$(basename "${url}")"
+
+	mkdir -p "${PACKAGES_PATH}"
+	pushd "${PACKAGES_PATH}" >/dev/null
+
+	if [[ ! -f "${package}" ]] || [[ "$(md5sum "${package}" | awk '{print $1}')" != "${md5sum}" ]]; then
+		log_info "Download ${package} ..."
+		curl -L "${url}" -o "${package}"
+		log_info "Download ${package} successfully!"
+	fi
+
+	popd >/dev/null
+}
+
+function download_all() {
+	download "${BINUTILS_PACKAGE_URL}" "${BINUTILS_MD5SUM}"
+	download "${LINUX_PACKAGE_URL}" "${LINUX_MD5SUM}"
+	download "${GLIBC_PACKAGE_URL}" "${GLIBC_MD5SUM}"
+	download "${GCC_PACKAGE_URL}" "${GCC_MD5SUM}"
+}
+
+function extract_packages() {
+	pushd "${PACKAGES_PATH}" >/dev/null
+
+	find . -mindepth 1 -maxdepth 1 -type d -exec rm -rf {} \;
+	for package in *.tar.xz; do
+		log_info "Extract ${package} ..."
+		tar -xf "${package}"
+		log_info "Extract ${package} successfully!"
+	done
+
+	popd >/dev/null
+}
+
+function build_binutils() {
+	local package
+	package="$(basename "${BINUTILS_PACKAGE_URL}")"
+	pushd "${package/.tar.xz/}" >/dev/null
+
+	mkdir -p "${CROSS_PREFIX}"
+	ln -snf .. "${CROSS_PREFIX}/${TARGET}"
+
+	rm -rf build
+	mkdir build
+	cd build
+
+	../configure --prefix="${CROSS_PREFIX}" \
+		--target="${TARGET}" \
+		--disable-multilib
+
+	make -j "$(nproc)"
+	make install
+
+	popd >/dev/null
+}
+
+function build_linux_headers() {
+	local package
+	package="$(basename "${LINUX_PACKAGE_URL}")"
+	pushd "${package/.tar.xz/}" >/dev/null
+
+	make ARCH="${ARCH}" INSTALL_HDR_PATH="${TARGET_PREFIX}" headers_install
+
+	popd >/dev/null
+}
+
+function build_gcc_step1() {
+	local package
+	package="$(basename "${GCC_PACKAGE_URL}")"
+	pushd "${package/.tar.xz/}" >/dev/null
+
+	contrib/download_prerequisites
+	mkdir -p "${TARGET_PREFIX}/lib"
+	ln -snf "lib" "${TARGET_PREFIX}/lib64"
+
+	rm -rf build
+	mkdir build
+	cd build
+
+	../configure --prefix="${CROSS_PREFIX}" \
+		--target="${TARGET}" \
+		--libdir="${TARGET_PREFIX}/lib" \
+		--libexecdir="${TARGET_PREFIX}/libexec" \
+		--with-local-prefix="${TARGET_PREFIX}" \
+		--with-gxx-include-dir="${TARGET_PREFIX}/include/c++" \
+		--enable-languages=c,c++ \
+		--disable-multilib \
+		--disable-libssp \
+		--disable-threads \
+		--disable-libatomic \
+		--disable-libffi \
+		--disable-libgomp \
+		--disable-libitm \
+		--disable-libquadmath \
+		--disable-libquadmath-support \
+		--disable-libsanitizer \
+		--disable-bootstrap
+	make -j "$(nproc)" all-gcc
+	make install-gcc
+
+	popd >/dev/null
+}
+
+function build_gcc_step2() {
+	local package
+	package="$(basename "${GCC_PACKAGE_URL}")"
+	pushd "${package/.tar.xz/}" >/dev/null
+
+	cd build
+
+	make -j "$(nproc)" all-target-libgcc
+	make install-target-libgcc
+
+	popd >/dev/null
+}
+
+function build_gcc_step3() {
+	local package
+	package="$(basename "${GCC_PACKAGE_URL}")"
+	pushd "${package/.tar.xz/}" >/dev/null
+
+	cd build
+
+	make -j "$(nproc)"
+	make install
+
+	popd >/dev/null
+}
+
+function build_glibc_step1() {
+	local package
+	package="$(basename "${GLIBC_PACKAGE_URL}")"
+	pushd "${package/.tar.xz/}" >/dev/null
+
+	rm -rf build
+	mkdir build
+	cd build
+
+	../configure --prefix="${TARGET_PREFIX}" \
+		--build="${MACHTYPE}" \
+		--host="${TARGET}" \
+		--target="${TARGET}" \
+		--with-headers="${TARGET_PREFIX}"/include \
+		--disable-multilib \
+		libc_cv_forced_unwind=yes
+	make install-bootstrap-headers=yes install-headers
+	make -j "$(nproc)" csu/subdir_lib
+	install csu/crt1.o csu/crti.o csu/crtn.o "${TARGET_PREFIX}"/lib
+	"${TARGET}-gcc" -nostdlib -nostartfiles -shared -x c /dev/null -o "${TARGET_PREFIX}"/lib/libc.so
+	touch "${TARGET_PREFIX}"/include/gnu/stubs.h
+
+	popd >/dev/null
+}
+
+function build_glibc_step2() {
+	local package
+	package="$(basename "${GLIBC_PACKAGE_URL}")"
+	pushd "${package/.tar.xz/}" >/dev/null
+
+	cd build
+
+	make -j "$(nproc)"
+	make install
+
+	popd >/dev/null
+}
+
+function build_cross_toolchain() {
+	build_binutils
+	build_linux_headers
+	build_gcc_step1
+	build_glibc_step1
+	build_gcc_step2
+	build_glibc_step2
+	build_gcc_step3
+}
+
+function build_binutils_final() {
+	local package
+	package="$(basename "${BINUTILS_PACKAGE_URL}")"
+	pushd "${package/.tar.xz/}" >/dev/null
+
+	rm -rf build
+	mkdir build
+	cd build
+
+	../configure --prefix="${PREFIX}" \
+		--host="${TARGET}" \
+		--enable-gold \
+		--enable-plugins \
+		--disable-multilib
+
+	make -j "$(nproc)"
+	make install
+
+	# Remove the old files
+	rm -rf "${PREFIX}/lib/ldscripts"
+
+	popd >/dev/null
+
+}
+
+function build_gcc_final() {
+	local package
+	package="$(basename "${GCC_PACKAGE_URL}")"
+	pushd "${package/.tar.xz/}" >/dev/null
+
+	rm -rf build
+	mkdir build
+	cd build
+
+	LDFLAGS="-L${PREFIX}/lib -Wl,-rpath=${PREFIX}/lib -Wl,--dynamic-linker=$(find "${PREFIX}/lib" -name 'ld-linux-*')" \
+		../configure --prefix="${PREFIX}" \
+		--host="${TARGET}" \
+		--with-local-prefix="${PREFIX}" \
+		--enable-languages=c,c++ \
+		--disable-multilib \
+		--disable-libquadmath \
+		--disable-libquadmath-support
+	make -j "$(nproc)"
+
+	# Remove the old files
+	rm -r "${PREFIX}/include/c++"
+
+	make install
+
+	popd >/dev/null
+}
+
+function build_glibc_final() {
+	local package
+	package="$(basename "${GLIBC_PACKAGE_URL}")"
+	pushd "${package/.tar.xz/}" >/dev/null
+
+	rm -rf build
+	mkdir build
+	cd build
+
+	../configure --prefix="${PREFIX}" \
+		--with-headers="${PREFIX}"/include \
+		--disable-multilib
+	make -j "$(nproc)"
+	make install
+
+	popd >/dev/null
+}
+
+function build_final_toolchain() {
+	build_binutils_final
+	build_gcc_final
+	build_glibc_final
+}
+
+function build_all() {
+	pushd "${PACKAGES_PATH}" >/dev/null
+
+	build_cross_toolchain
+	build_final_toolchain
+
+	popd >/dev/null
 }
 
 function main() {
-	if [[ ! -d '/output' ]]; then
-		log_error "The host dirve wasn't mounted."
-	elif [[ -d '/output/x86_64-pc-linux-gnu' ]]; then
-		log_warning 'Toolchain was generated.'
-		exit 0
-	fi
+	setup_environment
+	show_environment
 
-	log_info 'Generating...'
+	download_all
+	extract_packages
+	build_all
 
-	prepare
-
-	# Clean up
-	cd /output/x86_64-pc-linux-gnu
-	rm -rf build.log.bz2
-
-	log_info 'Packaging...'
-	cd /output
-	tar -zcf x86_64-pc-linux-gnu.tgz x86_64-pc-linux-gnu
-
-	cat /x-tools/setup_toolchain.sh x86_64-pc-linux-gnu.tgz >install_toolchain.sh
-	chmod a+x install_toolchain.sh
-
-	log_info 'Completed!'
+	# Remove the cross toolchain
+	rm -rf "${CROSS_PREFIX}"
 }
 
 main "${@}"
