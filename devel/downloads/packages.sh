1928c311337cd079040e31cc989129c4a7be151c
Adonis Ling <adonis0147@gmail.com>
2025-12-16 23:54:16 +0800

    chore: bump the version of LLVM

--- devel/downloads/packages.sh
+++ devel/downloads/packages.sh
@@ -195,10 +195,10 @@ LIBEDIT_PACKAGE_SHA256SUM='23792701694550a53720630cd1cd6167101b5773adddcb4104f73
 LIBEDIT_PACKAGE_NAME='libedit-20250104-3.1.tar.gz'
 LIBEDIT_PACKAGE_EXTRACTED_DIR='libedit-20250104-3.1'
 
-LLVM_PACKAGE_URL='https://github.com/llvm/llvm-project/releases/download/llvmorg-21.1.7/llvm-project-21.1.7.src.tar.xz'
-LLVM_PACKAGE_SHA256SUM='e5b65fd79c95c343bb584127114cb2d252306c1ada1e057899b6aacdd445899e'
-LLVM_PACKAGE_NAME='llvm-project-21.1.7.src.tar.xz'
-LLVM_PACKAGE_EXTRACTED_DIR='llvm-project-21.1.7.src'
+LLVM_PACKAGE_URL='https://github.com/llvm/llvm-project/releases/download/llvmorg-21.1.8/llvm-project-21.1.8.src.tar.xz'
+LLVM_PACKAGE_SHA256SUM='4633a23617fa31a3ea51242586ea7fb1da7140e426bd62fc164261fe036aa142'
+LLVM_PACKAGE_NAME='llvm-project-21.1.8.src.tar.xz'
+LLVM_PACKAGE_EXTRACTED_DIR='llvm-project-21.1.8.src'
 
 ZSH_PACKAGE_URL='https://downloads.sourceforge.net/project/zsh/zsh/5.9/zsh-5.9.tar.xz'
 ZSH_PACKAGE_SHA256SUM='9b8d1ecedd5b5e81fbf1918e876752a7dd948e05c1a0dba10ab863842d45acd5'
